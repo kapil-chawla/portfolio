e334b9f04d09b022f51eae55b392fb9bcffe9da7
Kapil Chawla <er.kapilchawla@gmail.com>
2024-07-29 12:55:00 +0530

    adding google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,16 @@ import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Footer from "./components/Footer";
+import ReactGA from 'react-ga';
+import { useEffect } from "react";
+
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-Z787F9NQYX');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
+
   return (
     <>
       <div>
